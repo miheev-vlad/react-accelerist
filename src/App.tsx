6a4601b3c3a15a39522eb2e438d11803c6042ffe
Vladislav Mikheev <vmikheev@primeaspect.io>
2021-12-16 02:37:37 +0300

    [fix-dashboard_layout] Deleted unnecessary files from the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
 import GlobalStyle from './globalStyles';
 import { MainRoutes } from './navigation';
 
